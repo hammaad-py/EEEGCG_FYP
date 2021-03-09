b6c41b9b09290e15a47574d8130a4184323b7031
Mohammed Hammaad Mateen <myfriendhammad@gmail.com>
2021-03-09 13:04:14 +0530

    Add files via upload

---
+++ Install-Tensorflow-Python3.7.ps1
@@ -0,0 +1,241 @@
+# This is the link to download Python 3.7.0 from Python.org
+# See https://www.python.org/downloads/
+$pythonUrl = "https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe"
+
+# This is the directory that the exe is downloaded to
+$tempDirectory = "C:\temp_provision\"
+
+# Installation Directory
+# Some packages look for Python here
+$targetDir = "C:\Python37"
+
+# create the download directory and get the exe file
+$pythonNameLoc = $tempDirectory + "python370.exe"
+New-Item -ItemType directory -Path $tempDirectory -Force | Out-Null
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile($pythonUrl, $pythonNameLoc)
+
+
+#Creating object detection directory
+
+$working_dir = "C:\Object_detection\"
+
+New-Item -Path $working_dir -ItemType Directory
+
+
+# These are the silent arguments for the install of python
+# See https://docs.python.org/3/using/windows.html
+$Arguments = @()
+$Arguments += "/i"
+$Arguments += 'InstallAllUsers="1"'
+$Arguments += 'TargetDir="' + $targetDir + '"'
+$Arguments += 'DefaultAllUsersTargetDir="' + $targetDir + '"'
+$Arguments += 'AssociateFiles="1"'
+$Arguments += 'PrependPath="1"'
+$Arguments += 'Include_doc="1"'
+$Arguments += 'Include_debug="1"'
+$Arguments += 'Include_dev="1"'
+$Arguments += 'Include_exe="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'InstallLauncherAllUsers="1"'
+$Arguments += 'Include_lib="1"'
+$Arguments += 'Include_pip="1"'
+$Arguments += 'Include_symbols="1"'
+$Arguments += 'Include_tcltk="1"'
+$Arguments += 'Include_test="1"'
+$Arguments += 'Include_tools="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += 'Include_launcher="1"'
+$Arguments += "/passive"
+
+#Install Python
+Start-Process $pythonNameLoc -ArgumentList $Arguments -Wait
+
+Function Get-EnvVariableNameList {
+    [cmdletbinding()]
+    $allEnvVars = Get-ChildItem Env:
+    $allEnvNamesArray = $allEnvVars.Name
+    $pathEnvNamesList = New-Object System.Collections.ArrayList
+    $pathEnvNamesList.AddRange($allEnvNamesArray)
+    return ,$pathEnvNamesList
+}
+
+Function Add-EnvVarIfNotPresent {
+Param (
+[string]$variableNameToAdd,
+[string]$variableValueToAdd
+   ) 
+    $nameList = Get-EnvVariableNameList
+    $alreadyPresentCount = ($nameList | Where{$_ -like $variableNameToAdd}).Count
+    if ($alreadyPresentCount -eq 0)
+    {
+    [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
+    [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
+    [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User)
+        $message = "Enviromental variable added to machine, process and user to include $variableNameToAdd"
+    }
+    else
+    {
+        $message = 'Environmental variable already exists. Consider using a different function to modify it'
+    }
+    Write-Information $message
+}
+
+Function Get-EnvExtensionList {
+    [cmdletbinding()]
+    $pathExtArray =  ($env:PATHEXT).Split("{;}")
+    $pathExtList = New-Object System.Collections.ArrayList
+    $pathExtList.AddRange($pathExtArray)
+    return ,$pathExtList
+}
+
+Function Add-EnvExtension {
+Param (
+[string]$pathExtToAdd
+   ) 
+    $pathList = Get-EnvExtensionList
+    $alreadyPresentCount = ($pathList | Where{$_ -like $pathToAdd}).Count
+    if ($alreadyPresentCount -eq 0)
+    {
+        $pathList.Add($pathExtToAdd)
+        $returnPath = $pathList -join ";"
+        [System.Environment]::SetEnvironmentVariable('pathext', $returnPath, [System.EnvironmentVariableTarget]::Machine)
+        [System.Environment]::SetEnvironmentVariable('pathext', $returnPath, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable('pathext', $returnPath, [System.EnvironmentVariableTarget]::User)
+        $message = "Path extension added to machine, process and user paths to include $pathExtToAdd"
+    }
+    else
+    {
+        $message = 'Path extension already exists'
+    }
+    Write-Information $message
+}
+
+Function Get-EnvPathList {
+    [cmdletbinding()]
+    $pathArray =  ($env:PATH).Split("{;}")
+    $pathList = New-Object System.Collections.ArrayList
+    $pathList.AddRange($pathArray)
+    return ,$pathList
+}
+
+Function Add-EnvPath {
+Param (
+[string]$pathToAdd
+   ) 
+    $pathList = Get-EnvPathList
+    $alreadyPresentCount = ($pathList | Where{$_ -like $pathToAdd}).Count
+    if ($alreadyPresentCount -eq 0)
+    {
+        $pathList.Add($pathToAdd)
+        $returnPath = $pathList -join ";"
+        [System.Environment]::SetEnvironmentVariable('path', $returnPath, [System.EnvironmentVariableTarget]::Machine)
+        [System.Environment]::SetEnvironmentVariable('path', $returnPath, [System.EnvironmentVariableTarget]::Process)
+        [System.Environment]::SetEnvironmentVariable('path', $returnPath, [System.EnvironmentVariableTarget]::User)
+        $message = "Path added to machine, process and user paths to include $pathToAdd"
+    }
+    else
+    {
+        $message = 'Path already exists'
+    }
+    Write-Information $message
+}
+
+Add-EnvExtension '.PY'
+Add-EnvExtension '.PYW'
+Add-EnvPath 'C:\Python37\'
+Add-EnvPath 'C:\Python37\'
+Add-EnvPath 'C:\Python37\Scripts\'
+
+
+
+
+### Installing TesnsorFlow dependency
+### change here for 32 bit System
+$vc_redistURL = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
+$vc_redistLoc = $tempDirectory + "vc_redist.x64.exe"
+
+
+New-Item -ItemType directory -Path $tempDirectory -Force | Out-Null
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile($vc_redistURL, $vc_redistLoc)
+
+#Install redist
+Start-Process $vc_redistLoc -Wait
+
+$VS_toolsURL = "https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe?fixForIE=.exe"
+$VS_toolstLoc = $tempDirectory + "VS_tools.exe"
+New-Item -ItemType directory -Path $tempDirectory -Force | Out-Null
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile($VS_toolsURL, $VS_toolstLoc)
+
+#Install VStool
+Start-Process $VS_toolstLoc -Wait
+
+# Install a library using Pip
+python -m pip install --upgrade pip
+python -m pip install -U --pre tensorflow=="2.*"
+python -m pip install tf_slim
+python -m pip install pywin32==225
+python -m pip install pycocotools
+python -m pip install pandas
+python -m pip install tf-models-official
+python -m pip install opencv-python==3.4.11.43
+python -m pip install lvis
+
+
+
+### Downloading protobuff for model files extraction
+$protobuffURL = "https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protoc-3.13.0-win64.zip"
+$protobuffLoc = $tempDirectory + "protobuff.zip"
+
+New-Item -ItemType directory -Path $tempDirectory -Force | Out-Null
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile($protobuffURL, $protobuffLoc)
+
+
+
+
+Get-ChildItem -Path "C:\Object_detection" -Include *.* -File -Recurse | foreach { $_.Delete()}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+    {
+        param([string]$zipfile, [string]$outpath)
+
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+    }
+Unzip $protobuffLoc $working_dir
+
+Get-ChildItem -Path $tempDirectory -Include *.* -File -Recurse | foreach { $_.Delete()}
+
+
+
+### Cloning Github Ripo and extracting it
+
+$tf_git_URL = "https://github.com/tensorflow/models/archive/master.zip"
+$tf_git_Loc = $tempDirectory + "master.zip"
+
+New-Item -ItemType directory -Path $tempDirectory -Force | Out-Null
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile($tf_git_URL, $tf_git_Loc)
+
+Unzip $tf_git_Loc $working_dir
+
+Start-Process "C:/Object_detection/bin/protoc.exe" "C:\\Object_detection\\models-master\\research\\object_detection\\protos\\*.proto --python_out=."
+
+
+
+## Getting into directory for processing protec file
+cd "C:/Object_detection/models-master/research"
+
+Start-Process -FilePath "C:/Object_detection/bin/protoc.exe" -ArgumentList "object_detection/protos/*.proto --python_out=."
+
+# set PYTHONPATH=\C:\Object_detection\models-master\research\object_detection\:$PYTHONPATH
+# cp object_detection/packages/tf2/setup.py .
+# py.exe -m pip install --use-feature=2020-resolver .
+# py.exe .\setup.py install
